49413092951ae8daea4df28027148248048157ee
ersanne <K1j@3QePIVFz>
2019-05-06 19:46:51 +0100

    New ssl deployment with linuxserver/letsencrypt

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,11 +1,10 @@
 <template>
-  <v-footer>
-    <v-layout
-      align-center
-      justify-center
-    >
+  <v-footer
+    class="v-footer v-footer--absolute theme--dark blue-grey darken-3">
+    <div
+      class="layout wrap justify-center">
       <p>&copy; 2019 Erik Sanne</p>
-    </v-layout>
+    </div>
   </v-footer>
 </template>
 
