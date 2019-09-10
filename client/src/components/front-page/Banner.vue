b59fc72363d87a66b48de05f1ac60a6a32bbd32f
ersanne <erik@eriksanne.com>
2019-09-10 12:04:38 +0100

    Update banner and added profile pic

--- client/src/components/front-page/Banner.vue
+++ client/src/components/front-page/Banner.vue
@@ -7,8 +7,11 @@
                 column
                 justify-center
         >
-            <h1 class="display-2 mb-3">Erik Sanne</h1>
-            <h4 class="subheading">Software Engineer</h4>
+          <v-avatar class="mb-4" size="200">
+            <v-img class="avatar_img_border" src="profile_pic.jpg"></v-img>
+          </v-avatar>
+          <h1 class="display-2 mb-3">Erik Sanne</h1>
+          <h4 class="subheading">Software Engineer</h4>
         </v-layout>
     </v-parallax>
 </template>
@@ -23,5 +26,8 @@ export default {
   .v-parallax__image {
     transform: none !important;
     width: 100% !important;
+  }
+  .avatar_img_border {
+    border: 4px solid #fff3e0;
   }
 </style>
