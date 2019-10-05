fb98821a636d89940ec53cc41470940dafcb6595
ersanne <K1j@3QePIVFz>
2019-10-05 17:14:07 +0100

    Fix CV not working

--- client/src/components/front-page/Portfolio.vue
+++ client/src/components/front-page/Portfolio.vue
@@ -45,18 +45,18 @@
 </template>
 
 <script>
-  import SectionLayout from "../../views/SectionLayout";
-  export default {
-    name: 'Portfolio',
-    components: {SectionLayout},
-    data: () => ({
-      cards: [
-        {title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12},
-        {title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6},
-        {title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6}
-      ]
-    })
-  }
+import SectionLayout from '../../views/SectionLayout'
+export default {
+  name: 'Portfolio',
+  components: { SectionLayout },
+  data: () => ({
+    cards: [
+      { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+      { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 }
+    ]
+  })
+}
 </script>
 
 <style scoped>
